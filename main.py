ab254dffe29e115c7d1fdfa5a963aba167a65866
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2023-04-12 15:37:37 -0700

    CI/CD

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 from BigNumber import BigNumber
 
-# a = BigNumber("22.22")
-# b = BigNumber("222")
-# c = a*(b)
-
-n1 = BigNumber("5000.0") 
-n2 = BigNumber("50.2")
-c = n1 % n2
-
-print(f"Result: {c}")
+c = BigNumber("-66") + BigNumber("300")
+print(c)
